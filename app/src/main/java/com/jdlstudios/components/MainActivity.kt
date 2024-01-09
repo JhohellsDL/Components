72c54a5cf1dd3f982adab65986293bb787a6f1bc
Jhohells Erick Dianderas Lopez <45276003+JhohellsDL@users.noreply.github.com>
2024-01-09 14:10:50 -0500

    add progress cicle 1

--- app/src/main/java/com/jdlstudios/components/MainActivity.kt
+++ app/src/main/java/com/jdlstudios/components/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.jdlstudios.components
 
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
 import com.jdlstudios.components.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -10,5 +15,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+
+
+        binding.buttonAgain.setOnClickListener {
+            //val animation: Animation = AnimationUtils.loadAnimation(this, R.anim.progress_bar_animation)
+            //val animation2: Animation = AnimationUtils.loadAnimation(this, R.anim.progress_bar_animation_2)
+            //binding.progressBar.startAnimation(animation)
+
+            // Crea un ObjectAnimator para animar el progreso
+            val progressAnimator = ObjectAnimator.ofInt(binding.progressBar, "progress", 0, 60)
+            progressAnimator.duration = 1000 // Duración de la animación en milisegundos
+
+            progressAnimator.start()
+
+            val valueAnimator = ValueAnimator.ofInt(0, 60)
+            valueAnimator.duration = 1000 // Duración de la animación en milisegundos
+            valueAnimator.addUpdateListener {
+                val animatedValue = it.animatedValue as Int
+                binding.textTest.text = animatedValue.toString()
+            }
+
+            // Inicia la animación
+            valueAnimator.start()
+
+        }
+
+
     }
 }
